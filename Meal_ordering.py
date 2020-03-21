6e9154a78ec89e620c77ceebd22779860bb32cd0
coldhair <coldhair@163.com>
2020-03-21 23:15:50 +0800

    项目对应的国家标准导入3

---
+++ Meal_ordering.py
@@ -0,0 +1,53 @@
+from docx import Document
+from docx.oxml.ns import qn
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT, WD_UNDERLINE
+from docx.shared import Pt
+
+with open('Meal_ordering.txt', encoding='utf8') as file:
+    lines = file.readlines()
+    print(lines)
+    num = len(lines)
+doc = Document()
+doc.styles['Normal'].font.name = u'Times New Roman'
+doc.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
+
+for i in range(1):
+    doc.add_paragraph()
+
+p = doc.add_paragraph()
+p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+run = p.add_run('应急机动队员领餐签字表')
+run.font.size = Pt(21)
+
+p = doc.add_paragraph()
+p.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
+run = p.add_run('2020年3月21日午餐')
+run.font.size = Pt(18)
+
+if num % 13 == 0:
+    columns = num / 13
+else:
+    columns = num // 13 + 1
+
+table = doc.add_table(rows=14, cols= columns * 3, style='Table Grid')
+# table.cell(0, 0).width = Pt(30)  # 设置列宽
+# table.cell(0, 1).width = Pt(120)  # 设置列宽
+# table.cell(0, 2).width = Pt(30)  # 设置列宽
+# table.cell(0, 3).width = Pt(120)  # 设置列宽
+
+
+
+
+for x in range(columns):
+    row_cells = table.rows[0].cells
+    row_cells[x+0].text='序号'
+    row_cells[x+1].text = '姓名'
+    row_cells[x+2].text = '签收'
+    for i in range(1,13):
+        row_cells = table.rows[i].cells
+        row_cells[x+0].text=str(i)
+        row_cells[x+1].text = lines[i-1]
+        row_cells[x+2].text ='\t'
+
+
+doc.save('neww.docx')
