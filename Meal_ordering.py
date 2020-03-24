cd17f53a13121589da81c36ad1ce120b7c694cec
coldhair <coldhair@163.com>
2020-03-24 17:24:09 +0800

    定餐小程序

--- Meal_ordering.py
+++ Meal_ordering.py
@@ -36,18 +36,20 @@ table = doc.add_table(rows=14, cols= columns * 3, style='Table Grid')
 # table.cell(0, 3).width = Pt(120)  # 设置列宽
 
 
-
-
 for x in range(columns):
     row_cells = table.rows[0].cells
-    row_cells[x+0].text='序号'
-    row_cells[x+1].text = '姓名'
-    row_cells[x+2].text = '签收'
-    for i in range(1,13):
-        row_cells = table.rows[i].cells
-        row_cells[x+0].text=str(i)
-        row_cells[x+1].text = lines[i-1]
-        row_cells[x+2].text ='\t'
+    row_cells[x*3+0].text='序号'
+    row_cells[x*3+1].text = '姓名'
+    row_cells[x*3+2].text = '签收'
+    for i in range(1,num):
+        if i%13==0:
+            m,y=13,i//13-1
+        else:
+            m,y=i%13,i//13
+        row_cells = table.rows[m].cells
+        row_cells[y*3+0].text=str(i)
+        row_cells[y*3+1].text = lines[i-1].strip()
+        row_cells[y*3+2].text ='\t'
 
 
 doc.save('neww.docx')
